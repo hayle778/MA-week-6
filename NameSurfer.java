dfc756562ff37fbfc8206e9e0f7aa794f2f55285
Haylemelekot <54283343+hayle778@users.noreply.github.com>
2021-03-10 23:15:03 -0500

    HH

---
+++ NameSurfer.java
@@ -0,0 +1,71 @@
+import acm.program.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NameSurfer extends Program implements NameSurferConstants {
+
+	 private static final double BOX_WIDTH = 120;
+	 private static final double BOX_HEIGHT = 50;
+	
+	
+
+	public void init() {
+	// label is for writing the name inside the box or the board
+	 nameLabel = new JLabel("Name:");
+		add(nameLabel,NORTH);
+		
+	
+		nameField = new JTextField(30);
+	    add(nameField, NORTH);
+//	nameField.addActionListener(this);
+	
+	        addButton = new JButton("add");
+	         add(addButton, NORTH);
+	         
+	      removeButton = new JButton("Remove");
+	      add(removeButton, NORTH);
+	        
+	        
+	         clearButton = new JButton("Clear")	;
+	          add(clearButton, NORTH);
+	     //  	addActionListeners();
+		addActionListeners();
+}
+
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == addButton) {
+			
+			println("Add: "+ nameField.getText() );
+		}
+	
+		else if(e.getSource() == nameField) {
+			println("Add: "+ nameField.getText());
+			
+	
+		}
+		
+		else if(e.getSource() == clearButton){
+			println("clear:" + nameField.getText());
+		}
+	
+	}
+
+	
+	
+	
+	
+
+	    private JLabel nameLabel;
+		private JTextField nameField;
+		private JButton addButton;
+		private JButton clearButton;
+		
+		private JButton removeButton;
+
+	
+	
+	
+	
+}
+
